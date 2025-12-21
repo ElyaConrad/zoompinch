1c94290239a08f93fe104a582e0fe9731683827e
Maurice Conrad <conr.maur@gmail.com>
2025-12-22 00:30:56 +0100

    fix

--- pre_update.sh
+++ pre_update.sh
@@ -4,13 +4,13 @@ cd core
 NEW_CORE_VERSION=$(npm version patch)
 
 cd ../elements
-node --eval "const fs = require('fs'); const pkg = JSON.parse(fs.readFileSync('package.json')); pkg.dependencies['@zoompinch/core'] = process.argv[1].replace(/^v/, ''); fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2));" "$NEW_CORE_VERSION"
+node --eval "const fs = require('fs'); const pkg = JSON.parse(fs.readFileSync('package.json')); pkg.dependencies['@zoompinch/core'] = process.argv[1].replace(/^v/, '^'); fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2));" "$NEW_CORE_VERSION"
 NEW_ELEMENTS_VERSION=$(npm version patch)
 
 cd ../vue
-node --eval "const fs = require('fs'); const pkg = JSON.parse(fs.readFileSync('package.json')); pkg.dependencies['@zoompinch/core'] = process.argv[1].replace(/^v/, ''); fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2));" "$NEW_CORE_VERSION"
+node --eval "const fs = require('fs'); const pkg = JSON.parse(fs.readFileSync('package.json')); pkg.dependencies['@zoompinch/core'] = process.argv[1].replace(/^v/, '^'); fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2));" "$NEW_CORE_VERSION"
 NEW_VUE_VERSION=$(npm version patch)
 
 cd ../playground
-node --eval "const fs = require('fs'); const pkg = JSON.parse(fs.readFileSync('package.json')); pkg.dependencies['@zoompinch/vue'] = process.argv[1].replace(/^v/, ''); fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2));" "$NEW_VUE_VERSION"
+node --eval "const fs = require('fs'); const pkg = JSON.parse(fs.readFileSync('package.json')); pkg.dependencies['@zoompinch/vue'] = process.argv[1].replace(/^v/, '^'); fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2));" "$NEW_VUE_VERSION"
 
